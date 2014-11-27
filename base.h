6bd7c46328d82f90f022af2856d65ce9f0b6ab4d
Chris Veenboer <chris.veenboer@gmail.com>
2014-11-27 11:18:15 +0100

    Ugly hack to insert function name in printf

--- base.h
+++ base.h
@@ -12,4 +12,23 @@
 
 typedef unsigned char bool;
 
+// enjoy this ugly hack:
+// macro to auto insert function name in printf function
+// but this introduces extra " marks around the format string
+// so we dirtily strip those out.
+
+#define print( format, ... ) do {\
+char buf[200];\
+int pos=0;\
+sprintf(buf, "%s(): " #format, __func__, __VA_ARGS__ ); \
+	for(int i=0; i<200; i++) {\
+		if( buf[i] == '"' ) {\
+				continue;\
+		} \
+		buf[pos++] = buf[i];\
+	}\
+		buf[pos++] = '\0';\
+	puts(buf);\
+} while (0)
+
 #endif

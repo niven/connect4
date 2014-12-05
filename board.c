e3c417309460bb2bad30bf1ee376ddb8c0e7a3af
Chris Veenboer <chris.veenboer@gmail.com>
2014-12-05 10:31:00 +0100

    Removed call to crappy JOIN macro that was failing

--- board.c
+++ board.c
@@ -244,12 +244,6 @@ internal void update_winlines( board* b, int move_y, int move_x ) {
 	int square_index = COLS*move_y + move_x;
 //	printf("Square [%d,%d] = %d\n", move_x, move_y, square_index);
 //	printf("num winlines: %d\n", s2w[square_index][0]);
-
-	char* wstr;
-	JOIN( wstr, unsigned int, "%u", &s2w[square_index][1], &s2w[square_index][0], ", ");
-//	 = join( (uint64_t *) &s2w[square_index][1], s2w[square_index][0], ", ");
-//	printf("Winlines affected: %s\n", wstr);
-	free( wstr );
 	
 	unsigned char* winline = current == WHITE ? b->winlines->black : b->winlines->white;
 	char scratch[200];

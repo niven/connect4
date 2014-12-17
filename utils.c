4be38b8ee87375fb1fdcdfae6a0cf2285fa31a13
Chris Veenboer <chris.veenboer@gmail.com>
2014-12-17 13:31:11 +0100

    Now with 64 bit hashing

--- utils.c
+++ utils.c
@@ -9,11 +9,12 @@
 size_t hash(size_t i) {
 
 	size_t h = i;
-	h ^= h >> 16;
-	h *= 0x85ebca6b;
-	h ^= h >> 13;
-	h *= 0xc2b2ae35;
-	h ^= h >> 16;
+	h ^= h >> 33;
+	h *= 0xff51afd7ed558ccd;
+	h ^= h >> 33;
+	h *= 0xc4ceb9fe1a85ec53;
+	h ^= h >> 33;
+
 	return h;
 }
 
